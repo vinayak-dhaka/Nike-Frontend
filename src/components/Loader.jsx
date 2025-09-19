e61b81bc65b4cf457ae11fbf8c45d1374c027542
vinayak dhaka <vinayakdhaka94@gmail.com>
2025-09-19 23:39:18 +0530

    Loader update

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -32,7 +32,7 @@ const LoadingSpinner = () => {
         Request sent to backend for data call. Please wait while data is
         fetched. <br />
         <strong>NOTE:</strong> As the backend API is hosted on a free platform
-        called <em>Render</em>, it might take a few minutes. <br />⏳ Minimum
+        called <em>Render</em>, it might take a few minutes. <br /> Minimum
         waiting time: <strong>4 minutes</strong>
       </p>
 
